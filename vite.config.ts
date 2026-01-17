1a8bc33f54fc7b443011f227a229a897c6e3cecc
Alex Wilczewski <alex@Alexs-MacBook-Pro-4.local>
2026-01-17 17:05:42 -0500

    Setup custom domain: eclipse-color-sheet.alexwil.com - update base path and remove basename

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/eclipse-color-sheet/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
